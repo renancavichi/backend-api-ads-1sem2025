66a1073895e928d6db4d1ba479bf0cca40566cf4
Renan Cavichi <renancavichi@gmail.com>
2025-03-25 22:38:44 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@
 import express from 'express'
 import userRouter from './routers/userRouter.js'
 import propertyRouter from './routers/propertyRouter.js'
+import cors from 'cors'
 
 const app = express()
 const port = 3000
 
 // Middleware
+app.use(cors()) // libera as requisições cors para o navegador
 app.use(express.json()) // faz o parse do json e transforma em objeto no req.body
 
 app.use('/user', userRouter)
